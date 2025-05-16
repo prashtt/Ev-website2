a45554e298b36a714091997eb8b7df560ced7841
prashtt <prashantnarwar511@gmail.com>
2025-05-16 21:51:05 +0530

    fix: add Render deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT || 4173,  // Render ka port use karega ya default 4173
+    host: true,                      // 0.0.0.0 pe bind karega, Render ke liye zaroori
+    allowedHosts: ['ev-website2-1.onrender.com'], // apna Render domain yahan dalein
+  }
 })
